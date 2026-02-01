f58424efd5a313bf84849305ba1fad3101b0496b
Rcxzx <168901928+Rcxzx@users.noreply.github.com>
2026-02-01 18:30:26 +0700

    fixed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ตั้งค่าการเชื่อมต่อ (ใช้ Websocket Port 8000 สำหรับหน้าเว็บ)
-client = new Paho.MQTT.Client("broker.hivemq.com", 8000, "web_client_" + Math.random());
+client = new Paho.MQTT.Client("broker.hivemq.com", 8884, "web_client_" + Math.random());
 
 client.onMessageArrived = function (message) {
     console.log("Message Arrived: " + message.payloadString);
@@ -7,8 +7,12 @@ client.onMessageArrived = function (message) {
 };
 
 client.connect({
+    useSSL: true, // <--- จุดสำคัญ: ต้องเป็น true เพื่อให้ทำงานบน GitHub ได้
     onSuccess: function () {
-        console.log("Connected!");
-        client.subscribe("Suvimol77"); // ต้องตรงกับที่ ESP8266 ส่งมา
+        console.log("Connected to Secure MQTT!");
+        client.subscribe("Suvimol77");
+        },
+        onFailure: function (error) {
+            console.log("Connect Failed: " + error.errorMessage);
     }
 });
